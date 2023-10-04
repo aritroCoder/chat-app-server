d24b921b5f909561e85ee008cbb79a8af7ba7f7a
aritroCoder <ogocodinggod@gmail.com>
2023-10-04 16:18:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello world</h1>");
+});
+
+io.on('connection', (socket) => {
+    socket.on('new-user', (user) => {
+        socket.broadcast.emit('user', user)
+    });
+    socket.on('new-message', (message) => {
+        socket.broadcast.emit('message', message)
+    })
+});
+
+server.listen(5500, () => {
+    console.log('server running at http://localhost:5500');
+});
