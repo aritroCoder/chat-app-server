f4c72a824f514bd9e4199fdcc4351c6291a0c132
aritroCoder <ogcodinggod@gmail.com>
2023-11-08 00:12:18 +0530

    added video calling socket
    Signed-off-by: aritroCoder <ogcodinggod@gmail.com>

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { Server } from 'socket.io';
+import express from 'express'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
-const app = express();
-const server = createServer(app);
+const app = express()
+const server = createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'http://localhost:3000'
     },
-    maxHttpBufferSize: 3*1e7 // 30 MB
-});
+    maxHttpBufferSize: 3 * 1e7 // 30 MB
+})
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello world</h1>");
-});
+    res.send('<h1>Hello world</h1>')
+})
 
 io.on('connection', (socket) => {
     socket.on('new-user', (user) => {
-        socket.broadcast.emit('user', user)
-    });
+        console.log('user joined: ', user)
+        console.log('user id: ', socket.id)
+        socket.emit('user', socket.id)
+    })
     socket.on('new-message', (message) => {
         socket.broadcast.emit('message', message)
     })
-});
+    // video call
+    socket.on('call-user', (data) => {
+        socket.to(data.to).emit('call-made', {
+            offer: data.offer,
+            socket: socket.id
+        })
+    })
+})
 
 server.listen(5500, () => {
-    console.log('server running at http://localhost:5500');
-});
+    console.log('server running at http://localhost:5500')
+})
